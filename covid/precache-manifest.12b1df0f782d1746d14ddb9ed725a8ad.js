32bb5f533b1c334d5d6358388d17a0e1ac5c6860
Mubeen <patel.mubeen99@gmail.com>
2020-04-21 22:16:52 +0530

    deployment fix

---
+++ covid/precache-manifest.12b1df0f782d1746d14ddb9ed725a8ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f12b1f3f7a22e008995627c65d320cc",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "1ba7b038a85320dfdfb0",
+    "url": "/covid/static/css/main.d3b9b4aa.chunk.css"
+  },
+  {
+    "revision": "a01b01f28fd03b53a08d",
+    "url": "/covid/static/js/2.24c7d440.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid/static/js/2.24c7d440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ba7b038a85320dfdfb0",
+    "url": "/covid/static/js/main.f70473d5.chunk.js"
+  },
+  {
+    "revision": "d26d7f96c97a39edce6a",
+    "url": "/covid/static/js/runtime-main.ddf79b8b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid/static/media/covid.d7265326.png"
+  }
+]);
